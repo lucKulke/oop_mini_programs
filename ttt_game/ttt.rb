165fc4099e01927a9931defb193745fa8ab40bba
lucKulke <lucaskulke98@gmail.com>
2022-02-01 13:39:04 +0100

    first commit for ttt game

---
+++ ttt_game/ttt.rb
@@ -0,0 +1,34 @@
+
+class TTT
+  
+  def initialize
+  end
+  
+  def play
+  end
+end
+
+class Board
+  def initialize
+  end
+end
+
+class Square
+  
+  def initialize
+  end
+
+end
+
+class Player
+  
+  def initialize
+  end
+  
+  def mark
+  end
+
+end
+
+
+TTT.new.play
